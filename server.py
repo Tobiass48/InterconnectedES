aa1326047022a719cb8426d3d1762a9967ff67c0
Tobiass48 <120596819+Tobiass48@users.noreply.github.com>
2022-12-14 20:26:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,3 +5,15 @@
 #
 
 import socket
+serverPort = 12000
+serverSocket= socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((",serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+while True:
+  connectionSocket, addr= serverSocket.accept()
+  
+  sentence= connectionSocket.recv(1024).decode()
+  sentence_2 = sentence
+  connectionSocket.send(sentence_2.encode())
+  connectionSocket.close()
