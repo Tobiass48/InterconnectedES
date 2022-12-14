f48ba6d639f167c2de92e7892e61fceac8a49537
Tobiass48 <120596819+Tobiass48@users.noreply.github.com>
2022-12-14 20:21:32 +0100

    Update client.py

--- client.py
+++ client.py
@@ -6,3 +6,13 @@
 
 import socket
 import time
+
+serverName= 'servername'
+serverPort= 12000
+clientSocket= socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence=raw_input('Input Lowercase sentence:')
+clientSocket.send(sentence.encode())
+modifiedSentence= clientSocket.recv(1024)
+print('From Server:', modifiedSentence.decode())
+clientSocket.close()
